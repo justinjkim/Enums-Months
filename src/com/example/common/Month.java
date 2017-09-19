a44b24b1b411c06d5854e604f025f5c8dcc910a8
Justin Kim <jjkim928@gmail.com>
2017-09-19 16:31:40 -0400

    added the months

--- src/com/example/common/Month.java
+++ src/com/example/common/Month.java
@@ -1,4 +1,13 @@
 package com.example.common;
 
 public enum Month {
+    JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER;
+
+    private String englishName;
+
+
+
+    public String getEnglishName() {
+        return englishName;
+    }
 }
