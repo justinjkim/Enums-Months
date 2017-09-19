2e336315260eebb25ba17b2a13ef666e6a57de11
Justin Kim <jjkim928@gmail.com>
2017-09-19 16:52:51 -0400

    added switch statement

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -14,6 +14,34 @@ public class Main {
         Scanner input = new Scanner(System.in);
 
         ArrayList<Month> followingMonths = new ArrayList<>();
+        String month = input.nextLine();
+
+        switch(month) {
+            case "January":
+                followingMonths.add(Month.JANUARY);
+            case "February":
+                followingMonths.add(Month.FEBRUARY);
+            case "March":
+                followingMonths.add(Month.MARCH);
+            case "April":
+                followingMonths.add(Month.APRIL);
+            case "May":
+                followingMonths.add(Month.MAY);
+            case "June":
+                followingMonths.add(Month.JUNE);
+            case "July":
+                followingMonths.add(Month.JULY);
+            case "August":
+                followingMonths.add(Month.AUGUST);
+            case "September":
+                followingMonths.add(Month.SEPTEMBER);
+            case "October":
+                followingMonths.add(Month.OCTOBER);
+            case "November":
+                followingMonths.add(Month.NOVEMBER);
+            case "December":
+                followingMonths.add(Month.DECEMBER);
+        }
 
     }
 }
