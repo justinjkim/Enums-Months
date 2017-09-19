a902026353737dc3d29b1cda0ffc1a8e9bdb5830
Justin Kim <jjkim928@gmail.com>
2017-09-19 16:36:25 -0400

    ask user for input

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,8 +1,15 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        System.out.println("Please enter a month, e.g., January");
+        Scanner input = new Scanner(System.in);
+
+
     }
 }
